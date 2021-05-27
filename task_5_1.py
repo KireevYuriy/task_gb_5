92a441552f0147d8b0c8fc0939005274fe1314b3
Yuriy <kireev_juriyy@rambler.ru>
2021-05-27 23:14:30 +0800

    change cod

--- task_5_1.py
+++ task_5_1.py
@@ -1,15 +1,11 @@
 
-
 def numbers_gen(right_num):
+    """Функция-генератор нечетных чисел от 1 до right_num"""
     for num in range(1, right_num + 1, 2):
         yield num
 
 
 in_number = int(input('Введите число - правую границу генератора: '))
 odd_num_gen = numbers_gen(in_number)
-if in_number // 2 == 0:
-    for i in range(in_number // 2):
-        print(next(odd_num_gen))
-else:
-    for i in range(in_number // 2 + 1):
-        print(next(odd_num_gen))
+for i in range(1, in_number + 1, 2):
+    print(next(odd_num_gen))
