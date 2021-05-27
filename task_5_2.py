1fda30f60f2a42cf652f1a392afee7f0b112728d
Yuriy <kireev_juriyy@rambler.ru>
2021-05-27 23:23:48 +0800

    change the cod in task_5_2.py

--- task_5_2.py
+++ task_5_2.py
@@ -3,4 +3,4 @@ from itertools import islice
 
 right_num = int(input('Введите число - правую границу генератора: '))
 odd_numbers = (num for num in range(1, right_num + 1, 2))
-print(*islice(odd_numbers))
+print(*islice(odd_numbers, right_num // 2 + 1))
