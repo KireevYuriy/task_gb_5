d1fe24b27680f117b6a476687e550288c47f5d8f
Yuriy <kireev_juriyy@rambler.ru>
2021-05-30 00:39:05 +0800

    changed the program code, after reading the tips of classmates

--- task_5_3.py
+++ task_5_3.py
@@ -1,23 +1,9 @@
 
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Евгений', 'Игорь']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
-
-def gen_of_school(names, name_class):
-    """Генератор кортежей вида (<tutor>, <klass>)"""
-    counter_klass = 0
-    counter_name = 0
-    while counter_klass < len(name_class):
-        if counter_klass >= len(names):
-            yield None, name_class[counter_klass]
-            counter_klass += 1
-            counter_name += 1
-            break
-        else:
-            yield names[counter_name], name_class[counter_klass]
-            counter_klass += 1
-            counter_name += 1
-
-
-for gen in gen_of_school(tutors, klasses):
-    print(gen, type(gen))
+if len(tutors) > len(klasses):
+    for i in range(len(tutors) - len(klasses)):
+        klasses.append(None)
+school_list = ((name, klass) for name, klass in zip(tutors, klasses))
+print(*school_list, '\n', type(school_list))
